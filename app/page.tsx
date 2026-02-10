d693bde67ff36655fc406eed55620c54886d0810
Aiswaryaa Velumani <113757038+Aiswaryaa04@users.noreply.github.com>
2026-02-09 22:22:50 -0600

    Import Hero and Section components in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Section from "../components/Section";
 import StackedCards, { type StackCard } from "../components/StackedCards";
 
 const experience: StackCard[] = [
